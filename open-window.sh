4cc35b3130cf1ce14b8d515ef2b44724fe894f71
August Valera <4U6U57@gmail.com>
2017-12-01 21:28:13 -0800

    Add GitLab CI, BATS unit testing, and shellcheck
    GitLab CI only calls shellcheck and BATS
    BATS does not test much, only makes a folder
    Shellcheck is great, script now compliant

--- open-window.sh
+++ open-window.sh
@@ -9,19 +9,18 @@
 # @date 2017-11-23
 #
 
-Exe=$(basename $0 .sh)
+Exe=$(basename "$0" .sh)
 
 # Error function
 Error() {
-  echo "$Exe: ERROR: $@" >&2
+  echo "$Exe: ERROR: $*" >&2
   exit 1
 }
 Warning() {
-  echo "$Exe: WARNING: $@" >&2
+  echo "$Exe: WARNING: $*" >&2
 }
 
-Usage() {
-cat << _USAGE
+Usage="
 .TH man 1 \"$(date)\" \"1.0\" \"$Exe man page\"
 .SH NAME
 $Exe \- Windows Subsystem for Linux opening utility
@@ -36,35 +35,33 @@ displays this help page
 associates this script with this filetype with xdg-open
 .IP -d
 disassociates this script with this filetype with xdg-open
-_USAGE
-}
+"
 
 DeskFile=~/.local/share/applications/$Exe.desktop
-Desktop() {
-cat << _DESKTOP
+Desktop="
 [Desktop Entry]
 Name=Open Window
 Exec=open-window %u
 Type=Application
-_DESKTOP
-}
+"
+
 MakeDesktop() {
-  [[ ! -e $(dirname $DeskFile) ]] && mkdir --parents $(dirname $DeskFile)
-  Desktop > $DeskFile
+  [[ ! -e $(dirname "$DeskFile") ]] && mkdir --parents "$(dirname "$DeskFile")"
+  echo "$Desktop" >"$DeskFile"
 }
 
 # Check that we're on Windows Subsystem for Linux
-! grep -q "Microsoft" /proc/sys/kernel/osrelease &> /dev/null && Error "Could not detect Windows Subsystem"
+! grep -q "Microsoft" /proc/sys/kernel/osrelease &>/dev/null && Error "Could not detect Windows Subsystem"
 
 # Find windows path
-WinPath=""
+WinHome=""
 ConfigFile=~/.$Exe
 DefaultsFile=~/.mailcap
 if [[ -e $ConfigFile ]]; then
-  source $ConfigFile
+  source "$ConfigFile"
 else
   echo "Creating configuration file: $ConfigFile"
-  touch $ConfigFile
+  touch "$ConfigFile"
 fi
 ! [[ -e $DefaultsFile ]] && touch $DefaultsFile
 
@@ -78,19 +75,19 @@ if [[ -z $WinHome ]]; then
   echo "Select your Windows home folder:"
   select WinHome in $WinDisk/Users/*; do break; done
   [[ -z $WinHome ]] && Error "Could not find Windows home folder"
-  echo "WinHome=$WinHome" >> $ConfigFile
+  echo "WinHome=$WinHome" >>"$ConfigFile"
 fi
 
 # Check command line arguments
 while getopts "ha:d:w" Opt; do
   case $Opt in
     (h)
-      man <(Usage)
+      man <(echo "$Usage")
       ;;
     (a)
       File=$OPTARG
       [[ ! -e $File ]] && Error "File does not exist: $File"
-      Type=$(xdg-mime query filetype $File)
+      Type=$(xdg-mime query filetype "$File")
       TypeSafe="${Type//\//\\/}"
       echo "Associating type $Type with $Exe"
       sed -i "/$TypeSafe/d" $DefaultsFile
@@ -99,13 +96,13 @@ while getopts "ha:d:w" Opt; do
     (d)
       File=$OPTARG
       [[ ! -e $File ]] && Error "File does not exist: $File"
-      Type=$(xdg-mime query filetype $File)
+      Type=$(xdg-mime query filetype "$File")
       TypeSafe="${Type//\//\\/}"
       echo "Disassociating type $Type with $Exe"
       sed -i "/$TypeSafe.*open-window/d" $DefaultsFile
       ;;
     (w)
-      if echo $BROWSER | grep "$Exe" >/dev/null; then
+      if echo "$BROWSER" | grep "$Exe" >/dev/null; then
         Warning "$Exe is already set as BROWSER"
       else
         BashFile=~/.bashrc
@@ -114,9 +111,11 @@ while getopts "ha:d:w" Opt; do
         if grep "BROWSER=.*$Exe" $BashFile >/dev/null; then
           Error "$BashFile already adds $Exe to BROWSER, check it for problems or restart your Bash"
         else
-          echo >> $BashFile
-          echo "# Adding $Exe as a browser for Bash for Windows" >> $BashFile
-          echo "BROWSER=\$BROWSER:$Exe" >> $BashFile
+          {
+            echo;
+            echo "# Adding $Exe as a browser for Bash for Windows";
+            echo "BROWSER=\$BROWSER:$Exe";
+          } >>$BashFile
         fi
       fi
       ;;
@@ -133,19 +132,19 @@ if [[ ! -z $File ]]; then
   if [[ -e $File ]]; then
     # File or directory
     # Move file to Windows partition, if necessary
-    FilePath="$(readlink -f $File | sed 's/ /-/g')"
+    FilePath="$(readlink -f "$File" | sed 's/ /-/g')"
     if [[ $FilePath != $WinHome/* ]]; then
       [[ ! -f $FilePath ]] && Error "Directory not in Windows partition: $FilePath"
       Warning "File not in Windows partition: $FilePath"
       TempFolder=$WinHome/AppData/Local/Temp/$Exe
-      [[ ! -e $TempFolder ]] && echo "Creating temporary folder: $TempFolder" && mkdir --parents $TempFolder
-      FilePath="$TempFolder/$(basename $FilePath)"
+      [[ ! -e $TempFolder ]] && echo "Creating temporary folder: $TempFolder" && mkdir --parents "$TempFolder"
+      FilePath="$TempFolder/$(basename "$FilePath")"
       echo -n "Copying "
-      cp -v $File "$FilePath" || Error "Could not copy file, check that it's not open on Windows"
+      cp -v "$File" "$FilePath" || Error "Could not copy file, check that it's not open on Windows"
     fi
 
-    FileWin=$(echo $FilePath | cut -d "/" -f 3-)
-    FileWin="$(tr '[a-z]' '[A-Z]' <<< ${FileWin:0:1}):/${FileWin:1}"
+    FileWin=$(echo "$FilePath" | cut -d "/" -f 3-)
+    FileWin="$(tr '[:lower:]' '[:upper:]' <<< ${FileWin:0:1}):/${FileWin:1}"
     FileWin="$(tr '/' '\\' <<< $FileWin)"
   elif [[ $File == *://* ]]; then
     # Link
